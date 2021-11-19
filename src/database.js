a1ed844d614d3927ec53c2b03cbff49c3caf247f
Rodrigo Romualdo <rodrigo.s.romualdo@gmail.com>
2021-11-19 00:15:52 -0300

    Inicio

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 let db;
 
-MongoClient.connect('mongodb://localhost/trackstest', {useUnifiedTopology: true}, (err, client) => {
+MongoClient.connect('mongodb+srv://teste:teste12345@cluster0.g21sq.mongodb.net/fluents-dev?retryWrites=true&w=majority', {useUnifiedTopology: true}, (err, client) => {
   if (err) {
     console.log(err);
     process.exit(0);
